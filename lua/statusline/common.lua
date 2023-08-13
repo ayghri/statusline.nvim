b2d5a1dc562af44878a923c868a01c65ed2f217f
Ayoub Ghriss <dev@aghriss.net>
2023-08-13 03:23:38 +0200

    added line length indicator

--- lua/statusline/common.lua
+++ lua/statusline/common.lua
@@ -4,57 +4,57 @@ local M = {}
 
 -- numbers on severity or 0
 local function count_severity(severity)
-	local a = #vim.diagnostic.get(0, { severity = severity })
-	return ((a and a > 0) and a) or 0
+  local a = #vim.diagnostic.get(0, { severity = severity })
+  return ((a and a > 0) and a) or 0
 end
 
 -- cyclic time Counter
 M.get_cyclic_counter = function(max_counter)
-	local ms = vim.loop.hrtime() / 1000000
-	return math.floor(ms / 120) % max_counter
+  local ms = vim.loop.hrtime() / 1000000
+  return math.floor(ms / 120) % max_counter
 end
 
 -- same as python elements.join(char) but skips empty elements
 M.join = function(char, elements)
-	-- local result = ""
-	-- for i, e in ipairs(elements) do
-	-- if e ~= "" then
-	-- 		result = result .. e .. (((i < #elements) and char) or "")
-	-- 	end
-	-- end
-	-- return result
-	return table.concat(elements, char)
+  -- local result = ""
+  -- for i, e in ipairs(elements) do
+  -- if e ~= "" then
+  -- 		result = result .. e .. (((i < #elements) and char) or "")
+  -- 	end
+  -- end
+  -- return result
+  return table.concat(elements, char)
 end
 
 -- replace "%#?#A%#B#" -> "%#?#" .. left_p .. "A%#B#"
 -- .- for lazy match
 M.pad = function(text, left_p, right_p)
-	if text == "" then return "" end
-	return string.gsub(text, "(%%#.-#)(.*)", "%1" .. (left_p or " ") .. "%2")
-		.. (right_p or " ")
+  if text == "" then return "" end
+  return string.gsub(text, "(%%#.-#)(.*)", "%1" .. (left_p or " ") .. "%2")
+    .. (right_p or " ")
 end
 
 -- returns the mode string and its highlight group
 M.get_mode = function()
-	local m = vim.api.nvim_get_mode().mode
-	return { mode = Mods[m][1], highlight = "%#" .. Mods[m][2] .. "#" }
+  local m = vim.api.nvim_get_mode().mode
+  return { mode = Mods[m][1], highlight = "%#" .. Mods[m][2] .. "#" }
 end
 
 M.get_cwd = function() return fn.fnamemodify(fn.getcwd(), ":t") end
 
 -- returns the filename and find a icon for filetype, uses web-devicons
 M.file_info = function()
-	local icon = Statusline.opts.default_file_icon
-	local filename = (fn.expand("%") == "" and "") or fn.expand("%:t")
+  local icon = Statusline.opts.default_file_icon
+  local filename = (fn.expand("%") == "" and "") or fn.expand("%:t")
 
-	if filename ~= "" then
-		local devicons_present, devicons = pcall(require, "nvim-web-devicons")
-		if devicons_present then
-			local ft_icon = devicons.get_icon(filename)
-			if ft_icon ~= nil then icon = ft_icon end
-		end
-	end
-	return { name = filename, icon = icon }
+  if filename ~= "" then
+    local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+    if devicons_present then
+      local ft_icon = devicons.get_icon(filename)
+      if ft_icon ~= nil then icon = ft_icon end
+    end
+  end
+  return { name = filename, icon = icon }
 end
 
 M.filetype = function() return vim.bo.ft end
@@ -62,23 +62,23 @@ M.filetype = function() return vim.bo.ft end
 ------------------------------------------------------------------------
 ------------------------------ Git stuff -------------------------------
 M.get_branch = function()
-	if not vim.b.gitsigns_git_status then return "" end
-	if vim.b.gitsigns_status_dict.head ~= "" then
-		return vim.b.gitsigns_status_dict.head
-	else
-		return "?"
-	end
+  if not vim.b.gitsigns_git_status then return "" end
+  if vim.b.gitsigns_status_dict.head ~= "" then
+    return vim.b.gitsigns_status_dict.head
+  else
+    return "?"
+  end
 end
 
 M.get_changes = function()
-	if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then return nil end
+  if not vim.b.gitsigns_head or vim.b.gitsigns_git_status then return nil end
 
-	local git_status = vim.b.gitsigns_status_dict
-	return {
-		added = git_status.added or 0,
-		changed = git_status.changed or 0,
-		removed = git_status.removed or 0,
-	}
+  local git_status = vim.b.gitsigns_status_dict
+  return {
+    added = git_status.added or 0,
+    changed = git_status.changed or 0,
+    removed = git_status.removed or 0,
+  }
 end
 
 ------------------------------------------------------------------------
@@ -86,37 +86,37 @@ end
 
 -- return "" if no client, LSP if null-ls, client otherwise
 M.LSP_client = function()
-	if rawget(vim, "lsp") then
-		for _, client in ipairs(vim.lsp.get_active_clients()) do
-			if client.attached_buffers[vim.api.nvim_get_current_buf()] then
-				return (client.name == "null-ls" and "LSP") or client.name
-			end
-		end
-	end
-	return ""
+  if rawget(vim, "lsp") then
+    for _, client in ipairs(vim.lsp.get_active_clients()) do
+      if client.attached_buffers[vim.api.nvim_get_current_buf()] then
+        return (client.name == "null-ls" and "LSP") or client.name
+      end
+    end
+  end
+  return ""
 end
 
 -- get the counts of different diagnostics
 M.LSP_severities = function()
-	if not rawget(vim, "lsp") then return {} end
-	return {
-		error_count = count_severity(vim.diagnostic.severity.ERROR),
-		warning_count = count_severity(vim.diagnostic.severity.WARN),
-		hint_count = count_severity(vim.diagnostic.severity.HINT),
-		info_count = count_severity(vim.diagnostic.severity.INFO),
-	}
+  if not rawget(vim, "lsp") then return {} end
+  return {
+    error_count = count_severity(vim.diagnostic.severity.ERROR),
+    warning_count = count_severity(vim.diagnostic.severity.WARN),
+    hint_count = count_severity(vim.diagnostic.severity.HINT),
+    info_count = count_severity(vim.diagnostic.severity.INFO),
+  }
 end
 
 -- Messages from LSP (like loading, formatting...)
 M.LSP_message = function()
-	if not rawget(vim, "lsp") or vim.lsp.status then return "" end
-	local lsp = vim.lsp.util.get_progress_messages()[1]
-	if not lsp then return "" end
+  if not rawget(vim, "lsp") or vim.lsp.status then return "" end
+  local lsp = vim.lsp.util.get_progress_messages()[1]
+  if not lsp then return "" end
 
-	local msg = lsp.message or ""
-	local percentage = lsp.percentage or 0
-	local title = lsp.title or ""
-	return { message = msg, title = title, percentage = percentage }
+  local msg = lsp.message or ""
+  local percentage = lsp.percentage or 0
+  local title = lsp.title or ""
+  return { message = msg, title = title, percentage = percentage }
 end
 
 return M
--- lua/statusline/themes/vscode.lua
+++ lua/statusline/themes/vscode.lua
@@ -4,169 +4,165 @@ local C = require("statusline.common")
 local M = {}
 -- replace empty strings
 local function sub_empty(emp, sub)
-	sub = sub or "?"
-	if emp == "" or emp == nil then return sub end
-	return emp
+  sub = sub or "?"
+  if emp == "" or emp == nil then return sub end
+  return emp
 end
 
 M.mode = function()
-	local m = C.get_mode()
-	return C.join(" ", { m.highlight .. Statusline.opts.mode_icon, m.mode })
+  local m = C.get_mode()
+  return C.join(" ", { m.highlight .. Statusline.opts.mode_icon, m.mode })
 end
 
 M.file_info = function()
-	local info = C.file_info()
-	return C.join(" ", { "%#StLineFileInfo#" .. info.icon, sub_empty(info.name) })
+  local info = C.file_info()
+  return C.join(" ", { "%#StLineFileInfo#" .. info.icon, sub_empty(info.name) })
 end
 
 M.filetype = function()
-	return C.join(" ", { "%#StLineFileType#", sub_empty(C.filetype()) })
+  return C.join(" ", { "%#StLineFileType#", sub_empty(C.filetype()) })
 end
 
 -- Git stuff
 M.git_branch = function()
-	local branch = C.get_branch()
-	if branch ~= "" then
-		return C.join(
-			" ",
-			{ "%#StLineGitBranch#", Statusline.opts.git_branch_icon, branch }
-		)
-	end
-	return ""
+  local branch = C.get_branch()
+  if branch ~= "" then
+    return C.join(
+      " ",
+      { "%#StLineGitBranch#", Statusline.opts.git_branch_icon, branch }
+    )
+  end
+  return ""
 end
 
 M.git_changes = function()
-	if vim.o.columns < Statusline.opts.git_changes_min_width then return "" end
-	local changes = C.get_changes()
-	if not changes then return "" end
-	local highlights = {
-		added = "%#StLineLSPInfo#",
-		changed = "%#StLineLSPWarning#",
-		removed = "%#StLineLSPError#",
-	}
-	local icons = Statusline.opts.git_changes_icons
-	local result = {}
-	for k, v in pairs(changes) do
-		if v > 0 then
-			table.insert(result, highlights[k] .. icons[k])
-			table.insert(result, v)
-		end
-	end
-	return C.join(" ", result)
+  if vim.o.columns < Statusline.opts.git_changes_min_width then return "" end
+  local changes = C.get_changes()
+  if not changes then return "" end
+  local highlights = {
+    added = "%#StLineLSPInfo#",
+    changed = "%#StLineLSPWarning#",
+    removed = "%#StLineLSPError#",
+  }
+  local icons = Statusline.opts.git_changes_icons
+  local result = {}
+  for k, v in pairs(changes) do
+    if v > 0 then
+      table.insert(result, highlights[k] .. icons[k])
+      table.insert(result, v)
+    end
+  end
+  return C.join(" ", result)
 end
 
 -- LSP STUFF
 M.LSP_progress = function()
-	if vim.o.columns < Statusline.opts.LSP_progress_min_width then return "" end
-	local progress = C.LSP_message()
-	if progress == "" then return "" end
-	local spinners = Statusline.opts.LSP_spinners
-	local content = string.format(
-		" %%<%s %s %s (%s%%%%) ",
-		spinners[C.get_cyclic_counter(#spinners) + 1],
-		progress.title,
-		progress.message,
-		progress.percentage
-	)
-	-- trim the progress message if parameter is set
-	if vim.g.lsprogress_len then
-		content = string.sub(content, 1, vim.g.lsprogress_len)
-	end
+  if vim.o.columns < Statusline.opts.LSP_progress_min_width then return "" end
+  local progress = C.LSP_message()
+  if progress == "" then return "" end
+  local spinners = Statusline.opts.LSP_spinners
+  local content = string.format(
+    " %%<%s %s %s (%s%%%%) ",
+    spinners[C.get_cyclic_counter(#spinners) + 1],
+    progress.title,
+    progress.message,
+    progress.percentage
+  )
+  -- trim the progress message if parameter is set
+  if vim.g.lsprogress_len then
+    content = string.sub(content, 1, vim.g.lsprogress_len)
+  end
 
-	return "%#StLineLspProgress#" .. content
+  return "%#StLineLspProgress#" .. content
 end
 
 M.LSP_diagnostics = function()
-	if vim.o.columns <= 0 then return "" end
-	local counts = C.LSP_severities()
-	local icons = Statusline.opts.diagnostics_icons
-	local diagnostics = C.join(" ", {
-		"%#StLineLspError#" .. icons.error,
-		sub_empty(counts.error_count),
-	})
+  if vim.o.columns <= 0 then return "" end
+  local counts = C.LSP_severities()
+  local icons = Statusline.opts.diagnostics_icons
+  local diagnostics = C.join(" ", {
+    "%#StLineLspError#" .. icons.error,
+    sub_empty(counts.error_count),
+  })
 
-	diagnostics = C.join(" ", {
-		diagnostics,
-		"%#StLineLspWarning#" .. icons.warn,
-		sub_empty(counts.warning_count),
-	})
+  diagnostics = C.join(" ", {
+    diagnostics,
+    "%#StLineLspWarning#" .. icons.warn,
+    sub_empty(counts.warning_count),
+  })
 
-	if counts.hint_count ~= nil and counts.hint_count > 0 then
-		diagnostics = C.join(
-			" ",
-			{ diagnostics, "%#StLineLspHints#" .. icons.hint, counts.hint_count }
-		)
-	end
+  if counts.hint_count ~= nil and counts.hint_count > 0 then
+    diagnostics = C.join(
+      " ",
+      { diagnostics, "%#StLineLspHints#" .. icons.hint, counts.hint_count }
+    )
+  end
 
-	if counts.info_count ~= nil and counts.info_count > 0 then
-		diagnostics = C.join(
-			" ",
-			{ diagnostics, "%#StLineLspInfo#" .. icons.info, counts.info_count }
-		)
-	end
+  if counts.info_count ~= nil and counts.info_count > 0 then
+    diagnostics = C.join(
+      " ",
+      { diagnostics, "%#StLineLspInfo#" .. icons.info, counts.info_count }
+    )
+  end
 
-	return diagnostics
+  return diagnostics
 end
 
 function M.cursor_position()
-	if vim.o.columns < Statusline.opts.position_min_width then return "" end
-	-- if vim.o.columns > 140 then
-	return "%#StLineText#Ln %l/%L, Cl %c"
-	-- end
-	-- return ""
+  if vim.o.columns < Statusline.opts.position_min_width then return "" end
+  -- if vim.o.columns > 140 then
+  -- local cur_pos = v/im.api.nvim_win_get_cursor()
+  local max_col = #vim.api.nvim_get_current_line()
+  return "%#StLineText#Ln %l/%L, Cl %c/" .. max_col
+  -- end
+  -- return ""
 end
 
 function M.file_encoding()
-	local encoding = string.upper(vim.bo.fileencoding)
-	if encoding == "" then return encoding end
-	return C.join(" ", { "%#StLineEncode#", encoding })
+  local encoding = string.upper(vim.bo.fileencoding)
+  if encoding == "" then return encoding end
+  return C.join(" ", { "%#StLineEncode#", encoding })
 end
 
 function M.LSP_status()
-	local client = C.LSP_client()
-	if client == "" then return "" end
-	if vim.o.columns > 100 then
-		return C.join(
-			" ",
-			{ "%#StLineLspStatus#", Statusline.opts.LSP_icon, client }
-		)
-	end
-	return C.join(" ", { "%#StLineLspStatus#", Statusline.opts.LSP_icon, "LSP" })
+  local client = C.LSP_client()
+  if client == "" then return "" end
+  if vim.o.columns > 100 then
+    return C.join(" ", { "%#StLineLspStatus#", Statusline.opts.LSP_icon, client })
+  end
+  return C.join(" ", { "%#StLineLspStatus#", Statusline.opts.LSP_icon, "LSP" })
 end
 
 M.cwd = function()
-	if vim.o.columns > 85 then
-		return C.join(
-			" ",
-			{ "%#StLineCwd#", Statusline.opts.cwd_icon, C.get_cwd() }
-		)
-	end
-	return ""
+  if vim.o.columns > 85 then
+    return C.join(" ", { "%#StLineCwd#", Statusline.opts.cwd_icon, C.get_cwd() })
+  end
+  return ""
 end
 
 function M.run()
-	-- local modules = require("statusline.vscode_colored")
+  -- local modules = require("statusline.vscode_colored")
 
-	-- if opts.overriden_modules then
-	-- M = vim.tbl_deep_extend("force", M, opts.overriden_modules())
-	-- end
-	-- pad adds space inside the highlight delimeter
-	local left = C.join("", {
-		C.pad(M.mode()),
-		C.pad(M.file_info()),
-		C.pad(M.git_branch()),
-		C.pad(M.LSP_diagnostics()),
-	})
-	local center = M.LSP_progress()
-	local right = C.join(" ", {
-		C.pad(M.git_changes(), "", " |"),
-		M.cursor_position(),
-		M.file_encoding(),
-		M.filetype(),
-		M.LSP_status(),
-		M.cwd(),
-	})
-	return left .. "%=" .. center .. "%=" .. right
+  -- if opts.overriden_modules then
+  -- M = vim.tbl_deep_extend("force", M, opts.overriden_modules())
+  -- end
+  -- pad adds space inside the highlight delimeter
+  local left = C.join("", {
+    C.pad(M.mode()),
+    C.pad(M.file_info()),
+    C.pad(M.git_branch()),
+    C.pad(M.LSP_diagnostics()),
+  })
+  local center = M.LSP_progress()
+  local right = C.join(" ", {
+    C.pad(M.git_changes(), "", " |"),
+    M.cursor_position(),
+    M.file_encoding(),
+    M.filetype(),
+    M.LSP_status(),
+    M.cwd(),
+  })
+  return left .. "%=" .. center .. "%=" .. right
 end
 
 return M
